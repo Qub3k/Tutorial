62a7004e4b20aa7bbfa1a078e462d89c06a42d39
Qub3k <jakub.tadeusz.nawala@gmail.com>
2014-05-24 17:56:24 +0200

    Poprawa wciec

--- font.py
+++ font.py
@@ -1,32 +1,28 @@
 class Font:
-	def __init__(self,name):
-		self.letters = ()
-		self.name = name
+ def __init__(self,name):
+  self.letters = ()
+  self.name = name
 
 class FontLoader:
-	def loadFont(self, directory):
-		font = Font("fancy")
+ def loadFont(self, directory):
+  font = Font("fancy")
 
-		font.letters['O'] = r'''
-	 ______
-   (  __  )
-   ( (__) ) 
-   (______)	  
-
-   	'''
-   		font.letters['I'] = r'''
-
-   	 __
-   	(  )
-   	 ><
-   	(__)
-
-   	'''
-      font.letters['T'] = r'''
-
-       ______
-      (_    _) 
-         ><
-        (__)
-      '''
-      return font
+  font.letters['O'] = r''' 
+ ______
+(  __  )
+( (__) ) 
+(______)	  
+'''
+  font.letters['I'] = r'''
+ __
+(  )
+ ><
+(__)
+'''
+  font.letters['T'] = r'''
+ ______
+(_    _) 
+   ><
+  (__)
+'''
+  return font
